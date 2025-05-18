db775d5a5f215a53bcb59562d8d408afef703aa2
Seif El_Sayed <seifsayed121@gmail.com>
2025-05-18 22:11:57 +0300

    booking route

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser')
 
 const userRouter = require('./routes/userRouter')
 const ownerHotelRouter = require('./routes/ownerHotelRouter') 
+const bookingRouter = require('./routes/bookingRouter')
 
 app.use(express.json())
 const allowedOrigins = ["http://localhost:5173"] 
@@ -18,6 +19,12 @@ app.use(cookieParser())
 
 app.use("/api/user", userRouter)
 app.use("/api/ownerHotel", ownerHotelRouter)
+app.use("/api/booking", bookingRouter)
+
+
+app.get('/', (req, res) => {
+    res.send("API is running")
+})
 
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("Connected to MongoDB"))
