ca82ae1d8ed2a793bd56962acfaceee9038a997f
Seif El_Sayed <seifsayed121@gmail.com>
2025-05-19 03:13:59 +0300

    assets

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const Home = () => {
+    return (
+        <div className="bg-[url('/src/assets/heroImage.png')] bg-cover 
+            bg-center h-screen flex items-center justify-center">
+    
+        </div>
+
+    )
+}
