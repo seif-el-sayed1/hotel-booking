8f2300ea5a34f537733dc7f876e490105d6d2176
Seif El_Sayed <seifsayed121@gmail.com>
2025-05-20 04:00:17 +0300

    user context

---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom'
+import { toast } from "react-toastify";
+
+
+export const UserContext = createContext();
+
+export const UserContextProvider = (props) => {
+    axios.defaults.withCredentials = true
+    const navigate = useNavigate()
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL; 
+    const [isLoggedin, setIsLoggedin] = useState(false);
+    const [userData, setUserData] = useState({});
+
+
+    const authState = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + "user/is-auth")
+            if(data.Success) {
+                setIsLoggedin(true)
+                getUserData()
+            } else {
+                navigate("/")
+                setUserData(false)
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+    
+    const getUserData = async () => {
+        const {data} = await axios.get(backendUrl + "user/getUser")
+        if(data.Success) {
+            setUserData(data.userData)
+            console.log(data.userData);
+        } else {
+            toast.error(data.message) 
+        }
+    }
+    const value = {
+        backendUrl,
+        isLoggedin, setIsLoggedin,
+        userData, setUserData,
+        getUserData,
+        authState
+    };
+
+    useEffect(() => {
+        authState()
+    },[])
+
+    return (
+        <UserContext.Provider value={value}>
+            {props.children}
+        </UserContext.Provider>
+    );
+};
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
+import { UserContextProvider } from "./context/UserContext.jsx";
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
