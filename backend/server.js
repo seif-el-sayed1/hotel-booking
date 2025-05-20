944b26173087af22047a3149f2b608801693a9ee
Seif El_Sayed <seifsayed121@gmail.com>
2025-05-20 03:58:08 +0300

    add uploads folder

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,13 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
+const path = require('path')
 
 const userRouter = require('./routes/userRouter')
 const ownerHotelRouter = require('./routes/ownerHotelRouter') 
 const bookingRouter = require('./routes/bookingRouter')
 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json())
 const allowedOrigins = ["http://localhost:5173"] 
 app.use(cors({
@@ -30,4 +32,4 @@ mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("Connected to MongoDB"))
 .catch((error) => console.log(error.message))
 
-app.listen(process.env.PORT || 3000, () => console.log("Server running on port 5000"))
+app.listen(process.env.PORT, () => console.log("Server running on port 5000"))
