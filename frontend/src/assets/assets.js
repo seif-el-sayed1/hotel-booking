136d47e47823492ba8c27874f50e51560eb629db
Seif El_Sayed <seifsayed121@gmail.com>
2025-05-27 15:01:16 +0300

    edit owner info

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -121,9 +121,9 @@ export const roomCommonData = [
 // User Dummy Data
 export const userDummyData = {
     "_id": "user_2unqyL4diJFP1E3pIBnasc7w8hP",
-    "username": "Great Stack",
+    "username": "Emma Rodriguez",
     "email": "user.greatstack@gmail.com",
-    "image": "https://img.clerk.com/eyJ0eXBlIjoicHJveHkiLCJzcmMiOiJodHRwczovL2ltYWdlcy5jbGVyay5kZXYvdXBsb2FkZWQvaW1nXzJ2N2c5YVpSSEFVYVUxbmVYZ2JkSVVuWnFzWSJ9",
+    "image": "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=200",
     "role": "hotelOwner",
     "createdAt": "2025-03-25T09:29:16.367Z",
     "updatedAt": "2025-04-10T06:34:48.719Z",
