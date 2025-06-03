23785cc6e7b8d97a27f7bc7712ea203de5a85d59
Seif El_Sayed <seifsayed121@gmail.com>
2025-06-04 01:44:35 +0300

    Remove unnecessary duration class from SideBar navigation items

--- frontend/src/components/owner/SideBar.jsx
+++ frontend/src/components/owner/SideBar.jsx
@@ -7,7 +7,7 @@ export const SideBar = () => {
     return (
         <div className='w-fit lg:w-1/5 border-r-1 border-gray-200 py-3 h-screen'>
             <div onClick={() => navigate("/owner")} 
-                className={`flex items-center gap-5 px-5 md:px-10 py-3 cursor-pointer duration-500 border-blue-600
+                className={`flex items-center gap-5 px-5 md:px-10 py-3 cursor-pointer border-blue-600
                                 ${window.location.pathname === "/owner" ?
                                                 "bg-blue-100 text-blue-600 border-r-7 border-blue-600" : "hover:bg-gray-200"}
                             `}>
@@ -15,7 +15,7 @@ export const SideBar = () => {
                 <p className='hidden md:block text-nowrap'>Dashboard</p>
             </div>
             <div onClick={() => navigate("/owner/add-room")} 
-                className={`flex items-center gap-5 px-5 md:px-10 py-3 cursor-pointer duration-500 border-blue-600
+                className={`flex items-center gap-5 px-5 md:px-10 py-3 cursor-pointer border-blue-600
                                 ${window.location.pathname === "/owner/add-room" ?
                                                 "bg-blue-100 text-blue-600 border-r-7 border-blue-600" : "hover:bg-gray-200"}
                             `}>
@@ -23,7 +23,7 @@ export const SideBar = () => {
                 <p className='hidden md:block text-nowrap'>Add Room</p>
             </div>
             <div onClick={() => navigate("/owner/list-room")} 
-                className={`flex items-center gap-5 px-5 md:px-10 py-3 cursor-pointer duration-500 border-blue-600
+                className={`flex items-center gap-5 px-5 md:px-10 py-3 cursor-pointer border-blue-600
                                 ${window.location.pathname === "/owner/list-room" ?
                                                 "bg-blue-100 text-blue-600 border-r-7 border-blue-600" : "hover:bg-gray-200"}
                             `}>
