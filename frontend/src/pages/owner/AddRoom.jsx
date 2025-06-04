7c6d569827440d7d69ee8677e23f6b9af81ee4b7
Seif El_Sayed <seifsayed121@gmail.com>
2025-06-05 01:09:05 +0300

    Implement room addition functionality with loading state management

--- frontend/src/pages/owner/AddRoom.jsx
+++ frontend/src/pages/owner/AddRoom.jsx
@@ -1,7 +1,20 @@
-import React, { useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { assets } from "../../assets/assets"
+import { UserContext } from "../../context/UserContext"
+import axios from "axios"
+import toast from "react-hot-toast"
 
 export const AddRoom = () => {
+    const {authState, backendUrl, loading, setLoading} = useContext(UserContext)
+    // const navigate = useNavigate()
+
+    useEffect(() => {
+        authState()    
+    },[])
+
+    
+
     const [images, setImages] = useState({
         1: null,
         2: null,
@@ -20,6 +33,53 @@ export const AddRoom = () => {
         }
     })
     
+    const handleSubmit = async (e) => {
+        try {
+            setLoading(true)
+            e.preventDefault();
+            const formData = new FormData();
+            formData.append("roomType", inputs.roomType);
+            formData.append("pricePerNight", inputs.pricePerNight);
+
+            const amenities = Object.keys(inputs.amenities).filter(key => inputs.amenities[key]);
+            formData.append("amenities", JSON.stringify(amenities));
+
+            Object.keys(images).forEach(key => {
+                images[key] && formData.append("images", images[key]);
+            });
+
+            const {data} = await axios.post(backendUrl + 'ownerHotel/add-room', formData)
+
+            if(data.success) {
+                toast.success(data.message)
+                setInputs({
+                    roomType: "",
+                    pricePerNight: 0,
+                    amenities: {
+                        "Free WiFi": false,
+                        "Free Breakfast": false,
+                        "Room Service": false,
+                        "Mountain View": false,
+                        "Pool Access": false
+                    }
+                })
+                setImages({
+                    1: null,
+                    2: null,
+                    3: null,
+                    4: null
+                })
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        } finally {
+            setLoading(false)
+        }
+
+    }
+    
     return (
         <div className='w-4/5'>
             <div className='py-10 pl-5 text-center lg:text-start'>
@@ -28,7 +88,16 @@ export const AddRoom = () => {
                     Fill in the details carefully and accurate room details, pricing, and amenities, to enhance the user booking experience.
                 </p>
             </div>
-            <form className='pb-5'>
+            {loading &&
+                <div className="fixed z-100 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                    <div className="flex flex-row gap-2">
+                        <div className="w-5 h-5 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]" />
+                        <div className="w-5 h-5 rounded-full bg-blue-700 animate-bounce [animation-delay:.3s]" />
+                        <div className="w-5 h-5 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]" />
+                    </div>
+                </div>
+            }
+            <form  onSubmit={handleSubmit} className='pb-5'>
                 <h2 className='pl-5 mb-2 text-black/80 text-lg'>Images</h2>
                 <div className='flex items-center flex-wrap gap-3 pl-5 mb-10'>
                     {Object.keys(images).map((ele, index) => {
@@ -72,7 +141,7 @@ export const AddRoom = () => {
                     {Object.keys(inputs.amenities).map((ele, index) => {
                         return (
                             <div key={index} className='pl-5 flex items-center gap-2'>
-                                <input type="checkbox" required id={index + 1} 
+                                <input type="checkbox" id={index + 1} 
                                         onChange={() => setInputs({...inputs, 
                                             amenities: {...inputs.amenities, [ele]: !inputs.amenities[ele]
                                         }})}
@@ -82,7 +151,9 @@ export const AddRoom = () => {
                         )
                     })}
                 </div>
-                <button className='text-white ml-5 mt-3 w-40 py-2 bg-blue-600 cursor-pointer rounded-md'>Add Room</button>
+                <button disabled={loading} className='text-white ml-5 mt-3 w-40 py-2 bg-blue-600 cursor-pointer rounded-md'>
+                    {loading ? 'Adding...' : 'Add Room'}
+                </button>
             </form>
         </div>
     )
