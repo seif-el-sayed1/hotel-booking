1f573291309ebddda1fe4b8a5c9124aacc29120e
Seif El_Sayed <seifsayed121@gmail.com>
2025-06-05 18:35:22 +0300

    Add AppContextProvider for room data management and loading state

---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { createContext, useState } from "react";
+import { toast } from "react-toastify";
+
+
+
+export const AppContext = createContext();
+
+export const AppContextProvider = (props) => {
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const [loading, setLoading] = useState(false);
+
+    const [allRooms, setAllRooms] = useState([])
+    
+    const getRoomsData = async () => {
+
+        try {
+            setLoading(true);
+            const { data } = await axios.get(backendUrl + "ownerHotel/get-rooms");
+            if (data.success) {
+                setAllRooms(data.rooms);
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+``
+    const value = {
+        getRoomsData,
+        allRooms,
+        loading
+    }
+    useEffect(() => {
+        getRoomsData();
+    }, [])
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
