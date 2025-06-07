96ec0de52590a1ce0618588af666f2ee9060c534
Seif El_Sayed <seifsayed121@gmail.com>
2025-06-07 23:08:10 +0300

    Implement search functionality with history tracking in Home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useContext, useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 import { assets, cities } from '../assets/assets'
+import { UserContext } from '../context/UserContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+
 
 export const Home = () => {
+    const navigate = useNavigate()
+
+    const {setSearchHistory, backendUrl} = useContext(UserContext)
+    const [destination, setDestination] = useState('')
+    
+
+    const onSearch = async (e) => {
+        e.preventDefault()
+        navigate(`/rooms?destination=${destination}`)
+        const  {data} =  await axios.post(backendUrl + "user/search-history", {destination})
+        if(data.Success) {
+            setSearchHistory((prev) => {
+                const updatedHistory = [...prev, destination]
+                if (updatedHistory.length > 3) {
+                    updatedHistory.shift()
+                } else {
+                    toast.error(data.message)
+                }
+                return updatedHistory
+            })
+        }
+
+    }
+
     return (
         <div className="flex flex-col gap-5 items-start justify-center px-6 md:px-16 lg:px-24
             xl:ox-32 text-white bg-no-repeat
@@ -17,13 +46,16 @@ export const Home = () => {
                     Unparalleled luxury and comfort await at the world's most exclusive hotels and resorts. Start your journey today.
                 </p>
             </div>
-            <form className=' bg-white text-gray-500 rounded-lg px-6 py-4  flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto'>
+            {/* Search */}
+            <form onSubmit={onSearch}
+                className=' bg-white text-gray-500 rounded-lg px-6 py-4  flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto'>
                 <div>
                     <div className='flex items-center gap-2'>
                         <img src={assets.calenderIcon} alt="calenderIcon" />
                         <label htmlFor="destinationInput">Destination</label>
                     </div>
-                    <input list='destinations' id="destinationInput" type="text" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" placeholder="Type here" required />
+                    <input onChange={(e) => setDestination(e.target.value)} value={destination}
+                        list='destinations' id="destinationInput" type="text" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" placeholder="Type here" required />
                     <datalist id='destinations'>
                         {cities.map((city, index) => (
                             <option key={index} value={city} />
