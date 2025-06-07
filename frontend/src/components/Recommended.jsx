321293e8ed6da4206b40fad513f2818611f51c37
Seif El_Sayed <seifsayed121@gmail.com>
2025-06-07 23:08:03 +0300

    Add Recommended component to display filtered hotel suggestions based on search history

---
+++ frontend/src/components/Recommended.jsx
@@ -0,0 +1,68 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+import { UserContext } from '../context/UserContext'
+import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+
+export const Recommended = () => {
+    const {allRooms} = useContext(AppContext)
+    const {searchHistory} = useContext(UserContext)
+
+    const navigate = useNavigate()
+
+    const [recommended, setRecommended] = useState([])
+
+    const filterHotels = async () => {
+        const filteredHotels = allRooms.slice().filter((room) => {
+            return searchHistory.includes(room.hotel.city)
+        })
+        setRecommended(filteredHotels)
+    }
+
+    useEffect(() => {
+        filterHotels()
+    }, [allRooms, searchHistory])
+
+    return recommended.length > 0 && (
+        <div className='pt-20 flex flex-col items-center justify-center bg-slate-50 ' >
+            <div className='flex flex-col items-center justify-center pb-15'>
+                <h1 className='font-light text-4xl mb-3'>Recommended Hotels</h1>
+                <p className='text-gray-500 text-center max-w-150'>Discover our handpicked selection of exceptional 
+                    properties around the world, offering unparalleled luxury and unforgettable experiences.</p>
+            </div>
+            <div className='flex flex-wrap items-center justify-center gap-5'>
+                {recommended.slice(0, 4).map((room) => {
+                    return (
+                        <div key={room._id} className='rounded-lg shadow-lg bg-white'>
+                            <img className='w-80 rounded-t-lg' src={room.images[0]} alt="room image" />
+                            <div className='flex items-center justify-between px-3 pt-4'>
+                                <p>{room.hotel.name}</p>
+                                <div className='flex items-center gap-1'>
+                                    <img src={assets.starIconFilled} alt="stat" />
+                                    <span className='text-gray-500'>4.5</span>
+                                </div>
+                            </div>
+                            <div className='flex items-center gap-2 px-3 pt-1'>
+                                <img src={assets.locationIcon} alt="location" />
+                                <p className='text-gray-500'>{room.hotel.address}</p>
+                            </div>
+                            <div className='flex items-center justify-between px-3 p-5'>
+                                <p className='text-xl'>${room.pricePerNight}<span className='text-gray-500 text-sm'>/night</span></p>
+                                <button onClick={() => navigate(`/rooms/${room._id}`)}
+                                    className='border-1 border-gray-500 hover:bg-gray-200 duration-200
+                                    rounded-md text-gray-500 p-2 text-sm cursor-pointer'>Book Now
+                                </button>
+                            </div>
+                        </div>
+                    )
+                })
+                }
+            </div>
+            <button onClick={() => {navigate('/rooms'); window.scrollTo({ top: 0, behavior: 'smooth' })}}
+                className='text-sm border-1 border-gray-300 hover:bg-gray-200 duration-200 px-2 py-1 
+                                cursor-pointer my-10 font-bold '>
+                View All Destinations
+            </button>    
+        </div>
+    ) 
+}
