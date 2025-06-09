7342277d59b3c8357a38d64e44422c441b291739
Seif El_Sayed <seifsayed121@gmail.com>
2025-06-09 03:39:20 +0300

    Refactor search form layout by removing unused date input fields

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -48,7 +48,7 @@ export const Home = () => {
             </div>
             {/* Search */}
             <form onSubmit={onSearch}
-                className=' bg-white text-gray-500 rounded-lg px-6 py-4  flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto'>
+                className=' bg-white text-gray-500 rounded-lg px-6 py-4   flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto'>
                 <div>
                     <div className='flex items-center gap-2'>
                         <img src={assets.calenderIcon} alt="calenderIcon" />
@@ -61,27 +61,6 @@ export const Home = () => {
                             <option key={index} value={city} />
                         ))}
                     </datalist>
-                </div>
-
-                <div>
-                    <div className='flex items-center gap-2'>
-                        <img src={assets.calenderIcon} alt="calender icon" />
-                        <label htmlFor="checkIn">Check in</label>
-                    </div>
-                    <input id="checkIn" type="date" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" />
-                </div>
-
-                <div>
-                    <div className='flex items-center gap-2'>
-                        <img src={assets.calenderIcon} alt="calender icon" />
-                        <label htmlFor="checkOut">Check out</label>
-                    </div>
-                    <input id="checkOut" type="date" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" />
-                </div>
-
-                <div className='flex md:flex-col max-md:gap-2 max-md:items-center'>
-                    <label htmlFor="guests">Guests</label>
-                    <input min={1} max={4} id="guests" type="number" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none  max-w-16" placeholder="0" />
                 </div>
 
                 <button className='flex items-center justify-center gap-1 rounded-md bg-black py-3 px-4 text-white my-auto cursor-pointer max-md:w-full max-md:py-1' >
