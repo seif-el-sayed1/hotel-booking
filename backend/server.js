d1cb4f4093eeb6a4e5230a2f7a05d6f8135c1f4f
Seif El_Sayed <seifsayed121@gmail.com>
2025-06-11 21:38:52 +0300

    update allowedOrigins

--- backend/server.js
+++ backend/server.js
@@ -10,17 +10,15 @@ const userRouter = require('./routes/userRouter');
 const ownerHotelRouter = require('./routes/ownerHotelRouter');
 const bookingRouter = require('./routes/bookingRouter');
 
-mongoose.connect(process.env.MONGO_URL)
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((error) => console.log(error.message));
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 
 const allowedOrigins = ["http://localhost:5173", 
-        "https://hotel-booking-frontend-sage.vercel.app",
-        "https://hotel-booking-frontend-git-main-seif-el-sayed1s-projects.vercel.app",
-        "https://hotel-booking-frontend-jik9x75j1-seif-el-sayed1s-projects.vercel.app"];
+        "https://hotel-booking-frontend-sage.vercel.app/",
+        "https://hotel-booking-frontend-git-main-seif-el-sayed1s-projects.vercel.app/",
+        "https://hotel-booking-frontend-jik9x75j1-seif-el-sayed1s-projects.vercel.app/",
+        "https://hotel-booking-frontend-kyocqzdp1-seif-el-sayed1s-projects.vercel.app/"];
 app.use(cors({
     origin: allowedOrigins,
     credentials: true
@@ -35,8 +33,11 @@ app.use("/api/booking", bookingRouter);
 app.get('/', (req, res) => {
     res.send("API is running");
 });
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((error) => console.log(error.message));
 
-// const port = process.env.PORT || 5000;
-// app.listen(port, () => console.log(`Server is running on port ${port}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 module.exports = app;
